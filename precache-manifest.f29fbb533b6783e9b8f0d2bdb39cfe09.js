873a432b41bab2fa8160e6727971a766a188616e
Christian <chris113377@gmail.com>
2020-01-24 11:09:54 -0600

    Updates

---
+++ precache-manifest.f29fbb533b6783e9b8f0d2bdb39cfe09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500960958df554297b5bcbc28a86ce9d",
+    "url": "/chris113377/SD260Assignment2/index.html"
+  },
+  {
+    "revision": "671cb8c0129d890757ca",
+    "url": "/chris113377/SD260Assignment2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c97f80a4a9e27f48b30",
+    "url": "/chris113377/SD260Assignment2/static/js/2.73306c6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chris113377/SD260Assignment2/static/js/2.73306c6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "671cb8c0129d890757ca",
+    "url": "/chris113377/SD260Assignment2/static/js/main.f7f666b5.chunk.js"
+  },
+  {
+    "revision": "60eb3c3e62bb21afb471",
+    "url": "/chris113377/SD260Assignment2/static/js/runtime-main.30a20598.js"
+  }
+]);
